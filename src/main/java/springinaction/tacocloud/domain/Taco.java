5cb3c4e163c5f65a4d5cdf1284e003970a4f7d3f
mamontov.vg <mamontov.vg@esphere.ru>
2020-11-22 21:15:29 +0300

    2.2 Done

---
+++ src/main/java/springinaction/tacocloud/domain/Taco.java
@@ -0,0 +1,11 @@
+package springinaction.tacocloud.domain;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Taco {
+    private String name;
+    private List<String> ingredients;
+}
